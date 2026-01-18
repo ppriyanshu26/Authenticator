0ab2871c45684e057abee1b674943a3d0194bf56
Priyanshu <172436044+ppriyanshu26@users.noreply.github.com>
2026-01-18 16:19:01 +0530

    Added app icon in title bar

--- Python/main.py
+++ Python/main.py
@@ -256,6 +256,7 @@ def build_lock_screen(root, otp_entries):
 if __name__ == "__main__":
     root = ctk.CTk()
     root.title("CipherAuth")
+    root.iconbitmap("icon.ico")
     root.geometry("420x550")
     root.configure(fg_color="#1e1e1e")
     root.resizable(False, False)
